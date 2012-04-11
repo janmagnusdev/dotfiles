5a576ed9b4cc5fb0cf2c746a5c5c8cf31866c785
Stefan Scherfke <stefan@sofa-rockers.org>
2012-04-11 10:17:20 +0200

    Updated plugin-update script.

--- update_vim.py
+++ update_vim.py
@@ -3,53 +3,23 @@ import os
 import os.path
 import subprocess
 
-git_bundles = [
-    'https://github.com/MarcWeber/vim-addon-mw-utils.git mw-utils',
-    'https://github.com/tomtom/tlib_vim.git tlib',
-    'https://github.com/garbas/vim-snipmate.git snipmate',
-    'https://github.com/ervandew/supertab.git supertab',
-    'https://github.com/tpope/vim-surround.git surround',
-    'https://github.com/wincent/Command-T.git command-t',
-    'https://github.com/mitechie/pyflakes-pathogen.git pyflakes',
-    'https://github.com/fs111/pydoc.vim.git pydoc',
-    'https://github.com/vim-scripts/pep8.git pep8',
-    'https://github.com/alfredodeza/pytest.vim.git py.test',
-    'https://github.com/vim-scripts/TaskList.vim.git tasklist',
-    'https://github.com/vim-scripts/The-NERD-tree.git nerdtree',
-    'https://github.com/sontek/rope-vim.git ropevim',
-    'https://github.com/vim-scripts/SearchComplete.git searchcomplete',
-]
-
-vim_org_scripts = [
-    'rename', 1928, 'plugin'
-    # 'name', 123, 'plugin',
-    # 'name', 456, 'syntax',
-]
 
 BUNDLE_DIR = '_vim/bundle'
 
 
-def clone():
-    os.makedirs(BUNDLE_DIR)
-    for repo in git_bundles:
-        subprocess.call(['git', 'clone'] + repo.split(' '), cwd=BUNDLE_DIR)
-
-    # Build comand-t
-    subprocess.call(['rake', 'make'], cwd=os.path.join(BUNDLE_DIR,
-            'command-t'))
-
-
 def update():
-    for repo in git_bundles:
-        local_dir = repo.split(' ')[1]
-        print('- Updading %s ...' % local_dir)
-        subprocess.call(['git', 'pull'],
-                cwd=os.path.join(BUNDLE_DIR, local_dir))
+    repo_types = {
+        '.git': 'git pull',
+        '.hg': 'hg pull -u',
+        '.svn': 'svn up',
+    }
+    for entry in os.listdir(BUNDLE_DIR):
+        for rtype, cmd in repo_types.items():
+            path = os.path.join(BUNDLE_DIR, entry)
+            if os.path.isdir(os.path.join(path, rtype)):
+                print('- Updating %s ...' % entry)
+                subprocess.call(cmd, cwd=path, shell=True)
 
 
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) == 2 and sys.argv[1] == 'clone':
-        clone()
-    else:
-        update()
+    update()
