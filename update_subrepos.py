d031866c02e1b051f3e9b46f4fdf1ec31dd3dcd3
Stefan Scherfke <stefan.scherfke@offis.de>
2012-08-14 13:45:26 +0200

    Rewrote update_vim.py and renamed it to update_subrepos.py.

---
+++ update_subrepos.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import re
+import subprocess
+
+
+repo = re.compile(r'\[(\w+)\](.*)')
+commands = {
+    'hg': 'hg pull -u',
+    'git': 'git pull',
+    'svn': 'svn up',
+}
+
+for line in open('.hgsub'):
+    if not line.strip():
+        continue
+
+    local_dir, repo_def = line.split(' = ')
+    repo_type, repo_url = repo.match(repo_def).groups()
+
+    print('- Updating %s ...' % local_dir)
+    subprocess.call(commands[repo_type], cwd=local_dir, shell=True)
