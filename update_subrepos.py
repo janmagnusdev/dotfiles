84e3dacc4a466f7d8641e40ddad3967055b52518
Stefan Scherfke <stefan@sofa-rockers.org>
2014-01-08 08:50:13 +0100

    Updated subrepos and update script.

--- update_subrepos.py
+++ update_subrepos.py
@@ -6,8 +6,7 @@ import subprocess
 repo = re.compile(r'\[(\w+)\](.*)')
 commands = {
     'hg': 'hg pull -u',
-    'git': 'git pull; git submodule update',
-    'svn': 'svn up',
+    'git': 'git pull; git checkout master; git submodule update',
 }
 
 for line in open('.hgsub'):
