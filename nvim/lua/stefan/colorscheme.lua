1b04876aa5ed40117332c36bdb94f01461677df4
Stefan Scherfke <stefan@sofa-rockers.org>
2023-03-30 22:28:18 +0200

    nvim: Improve timer for dark mode detection

--- nvim/lua/stefan/colorscheme.lua
+++ nvim/lua/stefan/colorscheme.lua
@@ -1,23 +1,27 @@
 local colorscheme = "stylo"
 local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not ok then
-   vim.notify("colorscheme " .. colorscheme .. " not found!")
-   return
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
 end
 
 -- "opt.background" detects the current system dark/light mode.
 -- Don't set it explicitly!
 --
--- Detect OS dark/light mode by running the following function every 3s:
--- TODO: Maybe replace with an async lua job?
+-- Detect OS dark/light mode by running the following function every 2s:
 local function check_is_dark_mode()
-   local os_mode = vim.fn.systemlist("dm get")[1]
-   if vim.opt.background:get() ~= os_mode then
-      vim.opt.background = os_mode
-   end
+  vim.fn.jobstart({ "dm", "get" }, {
+    stdout_buffered = true,
+    on_stdout = function(_, data, _)
+      local os_mode = table.concat(data)
+      if vim.opt.background:get() ~= os_mode then
+        vim.opt.background = os_mode
+      end
+    end,
+  })
 end
-check_is_dark_mode()
-vim.fn.timer_start(3000, check_is_dark_mode, { ["repeat"] = -1 })
+local timer = vim.loop.new_timer()
+timer:start(0, 2000, vim.schedule_wrap(check_is_dark_mode))
 
 -- Reload the colorscheme whenever we write the file.
 vim.cmd([[
