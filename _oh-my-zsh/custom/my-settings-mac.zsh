d40496fd92cf3ff98449cffa90933270ce660f62
Jan-Magnus Monenschein <mail@janmagnus.dev>
2024-06-10 09:56:51 +0200

    add viman alias also for mac

--- _oh-my-zsh/custom/my-settings-mac.zsh
+++ _oh-my-zsh/custom/my-settings-mac.zsh
@@ -7,4 +7,9 @@ echo "$(basename $0) is being sourced..."
 # Cargo Bins
 PATH="${PATH:+${PATH}:}/Users/jan-magnus/.cargo/bin"
 
+if command -v viman &> /dev/null
+then
+  alias man="viman"
+fi
+
 echo "$(basename $0) end..."
