8b8a47b15aaa11404413c12063d65d7eec050cd2
Jan-Magnus Monenschein <me@janmagnus.dev>
2024-07-23 14:27:21 +0200

    remove miniforge

--- _oh-my-zsh/custom/my-settings-egs.zsh
+++ _oh-my-zsh/custom/my-settings-egs.zsh
@@ -4,31 +4,6 @@ fi
 
 echo "$(basename $0) is being sourced..."
 
-emsconda-init() {
-  # Allow activation and usage emsconda env
-  export EMSCONDA_DIR="${HOME}/emsconda"
-  source "${EMSCONDA_DIR}/etc/profile.d/ems_rc.sh"
-  # Include default aliases like dev, ca, da (Optional)
-  source "${EMSCONDA_DIR}/etc/profile.d/ems_alias.sh"
-}
-
-miniforge-init() {
-  # >>> conda initialize >>>
-  # !! Contents within this block are managed by 'conda init' !!
-  __conda_setup="$('/home/jan-magnus/miniforge3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-  if [ $? -eq 0 ]; then
-      eval "$__conda_setup"
-  else
-      if [ -f "/home/jan-magnus/miniforge3/etc/profile.d/conda.sh" ]; then
-          . "/home/jan-magnus/miniforge3/etc/profile.d/conda.sh"
-      else
-          export PATH="/home/jan-magnus/miniforge3/bin:$PATH"
-      fi
-  fi
-  unset __conda_setup
-  # <<< conda initialize <<<
-}
-
 alias emsconda-dockerbuild="docker pull emsforge.services.ems/emsconda-develop:latest && \
     docker run -it --rm \
     --volume=/etc/emsencrypt:/etc/emsencrypt \
@@ -38,5 +13,14 @@ alias emsconda-dockerbuild="docker pull emsforge.services.ems/emsconda-develop:l
 
 # Go
 PATH="${PATH:+${PATH}:}/home/jan-magnus/.local/bin/go/bin"
+
+emsconda-init() {
+  # Allow activation and usage emsconda env
+  export EMSCONDA_DIR="${HOME}/emsconda"
+  source "${EMSCONDA_DIR}/etc/profile.d/ems_rc.sh"
+  # Include default aliases like dev, ca, da (Optional)
+  source "${EMSCONDA_DIR}/etc/profile.d/ems_alias.sh"
+}
+emsconda-init
 
 echo "$(basename $0) end..."
