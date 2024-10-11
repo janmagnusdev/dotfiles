433a0f999736028e3b1e233fc2afc4952f39f855
Jan-Magnus Monenschein <mail@janmagnus.dev>
2024-10-11 16:05:58 +0200

    added pnpm to mac configuratuon

--- _oh-my-zsh/custom/my-settings-mac.zsh
+++ _oh-my-zsh/custom/my-settings-mac.zsh
@@ -12,4 +12,12 @@ then
   alias man="viman"
 fi
 
+# pnpm
+export PNPM_HOME="/Users/jan-magnus/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+
 echo "$(basename $0) end..."
