99394a081bc82c3f0e6d695877f1cfa003729def
Jan-Magnus Monenschein <me@janmagnus.dev>
2024-11-24 14:18:52 +0100

    add specific ls aliases

--- _oh-my-zsh/custom/aliases.zsh
+++ _oh-my-zsh/custom/aliases.zsh
@@ -1,2 +1,8 @@
 alias git-search-rename="git log -M --summary | grep rename | grep"
 alias keyadd="ssh-add ~/.ssh/id_rsa"
+
+# ls
+unalias l
+unalias ls
+alias ls='ls --color=tty -h'
+alias l="ls -lh"
